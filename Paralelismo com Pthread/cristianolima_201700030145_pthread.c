e917572044a1834cba629bd6b1a471a45272b927
Cristiano Lima Oliveira <59293960+CristianoLimaOliveira@users.noreply.github.com>
2021-09-29 11:32:22 -0300

    Carregando dados para struct. A struct será utilizada para passar mais de um argumento por parâmetro.

--- Paralelismo com Pthread/cristianolima_201700030145_pthread.c
+++ Paralelismo com Pthread/cristianolima_201700030145_pthread.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <pthread.h>
+
+struct estruturaPorThread
+{
+    uint64_t quantidadeDeLinhasMatriz1;
+    uint64_t quantidadeDeColunasMatriz1;
+    uint64_t quantidadeDeLinhasMatriz2;
+    uint64_t quantidadeDeColunasMatriz2;
+    double *ptrMatriz1;
+    double *ptrMatriz2;
+    double *ptrMatrizResultante;
+    uint32_t indicaLinhaMatriz1;
+    uint32_t indicaColunaMatriz2;
+};
+
 /* ESBOÇO
 void multiplicaVetores(int indicaLinhaMatriz1, int indicaColunaMatriz2, int quantidadeDeLinhasMatriz1, int quantidadeDeColunasMatriz1, int quantidadeDeLinhasMatriz2, int quantidadeDeColunasMatriz2, double *ptrMatriz1, double *ptrMatriz2, double *ptrMatrizResultante){
     double result = 0;
@@ -17,6 +31,8 @@ void multiplicaVetores(int indicaLinhaMatriz1, int indicaColunaMatriz2, int quan
 int main(int argc, char *argv[]){
     uint64_t quantidadeDeMultiplicacoes, quantidadeDeLinhasMatriz1, quantidadeDeLinhasMatriz2, quantidadeDeColunasMatriz1, quantidadeDeColunasMatriz2, quantidadeDeElementosMatriz1, quantidadeDeElementosMatriz2, quantidadeDeElementosMatrizResultante;
     double *ptrMatriz1=NULL, *ptrMatriz2=NULL, *ptrMatrizResultante=NULL;
+    uint32_t indexArrayDados;
+    struct estruturaPorThread *ptrDados=NULL;
 
     printf("%s\n%s\n", argv[1], argv[2]);
 
@@ -53,13 +69,38 @@ int main(int argc, char *argv[]){
 
         quantidadeDeElementosMatrizResultante = quantidadeDeLinhasMatriz1 * quantidadeDeColunasMatriz2;
         ptrMatrizResultante = malloc(quantidadeDeElementosMatrizResultante * sizeof(double));
+        ptrDados = malloc(quantidadeDeElementosMatrizResultante * sizeof(struct estruturaPorThread));
+
+        indexArrayDados = 0;
+        for(uint32_t contadorSegundoFor = 0; contadorSegundoFor < quantidadeDeColunasMatriz2; contadorSegundoFor++){
+            for(uint32_t contadorTerceiroFor = 0; contadorTerceiroFor < quantidadeDeLinhasMatriz1; contadorTerceiroFor++){
+                ptrDados[indexArrayDados].quantidadeDeLinhasMatriz1 = quantidadeDeLinhasMatriz1;
+                ptrDados[indexArrayDados].quantidadeDeColunasMatriz1 = quantidadeDeColunasMatriz1;
+                ptrDados[indexArrayDados].quantidadeDeLinhasMatriz2 = quantidadeDeLinhasMatriz2;
+                ptrDados[indexArrayDados].quantidadeDeColunasMatriz2 = quantidadeDeColunasMatriz2;
+                ptrDados[indexArrayDados].ptrMatriz1 = ptrMatriz1;
+                ptrDados[indexArrayDados].ptrMatriz2 = ptrMatriz2;
+                ptrDados[indexArrayDados].ptrMatrizResultante = ptrMatrizResultante;
+                ptrDados[indexArrayDados].indicaLinhaMatriz1 = contadorTerceiroFor;
+                ptrDados[indexArrayDados].indicaColunaMatriz2 = contadorSegundoFor;
+                indexArrayDados += 1;
+            }
+        }
+
+        for(uint32_t contadorSegundoFor = 0; contadorSegundoFor < quantidadeDeElementosMatrizResultante; contadorSegundoFor++){
+            printf("%u %lu %lu %lu %lu %u %u\n", contadorSegundoFor, ptrDados[contadorSegundoFor].quantidadeDeLinhasMatriz1, ptrDados[contadorSegundoFor].quantidadeDeColunasMatriz1, ptrDados[contadorSegundoFor].quantidadeDeLinhasMatriz2, ptrDados[contadorSegundoFor].quantidadeDeColunasMatriz2, ptrDados[contadorSegundoFor].indicaLinhaMatriz1, ptrDados[contadorSegundoFor].indicaColunaMatriz2);
+        }
+        
 
         free(ptrMatriz1);
         free(ptrMatriz2);
         free(ptrMatrizResultante);
+        free(ptrDados);
         ptrMatriz1=NULL;
         ptrMatriz2=NULL;
         ptrMatrizResultante=NULL;
+        ptrDados=NULL;
+        printf("\n");
     }
 
     fclose(fp_input);
